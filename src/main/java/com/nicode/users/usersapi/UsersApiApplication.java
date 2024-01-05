ccdb958f6030e58f392530c4c1a868ec59485b7b
Nicode <alenolmedo92@gmail.com>
2024-01-05 01:43:07 -0300

    no message

--- src/main/java/com/nicode/users/usersapi/UsersApiApplication.java
+++ src/main/java/com/nicode/users/usersapi/UsersApiApplication.java
@@ -2,8 +2,10 @@ package com.nicode.users.usersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
